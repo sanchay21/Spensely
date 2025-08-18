cec1d2096ac8aaea88e2ab5626a748b325c9c5df
Sanchay Singh <sanchaysingh21@gmail.com>
2025-08-18 19:50:13 +0530

    Added Edit And Modal Form

--- core/views.py
+++ core/views.py
@@ -16,26 +16,59 @@ def transactions_add(request):
 
     if (request.method == "POST"):
         amount = request.POST.get('amount')
-        desc = request.POST.get('desc')
+        desc = request.POST.get('description')
         date = request.POST.get('date')
+        category = request.POST.get('category')
 
         transaction = Transactions.objects.create(
             user = request.user,
             amount = amount,
             description = desc,
-            date = date
+            date = date,
+            category = category
         )
         print("Transaction Added !")
         return JsonResponse({
             "success": True,
             "id": transaction.id,
             "date": str(transaction.date),
-            "category": getattr(transaction, "category", None) or "-",
+            "category": transaction.category,
             "description": transaction.description,
             "amount": transaction.amount
         })
         
     return JsonResponse({"success": False}, status=400)
+
+def transactions_edit(request, id):
+    if(request.method == "POST"):
+        amount = request.POST.get('amount')
+        desc = request.POST.get('description')
+        date = request.POST.get('date')
+        category = request.POST.get('category')
+
+        transaction = Transactions.objects.get(id=id)
+        if(not transaction):
+            return JsonResponse({"success": False, "error": "Transaction does'nt exist"}, status=400)
+        
+        transaction.amount = amount
+        transaction.description = desc
+        transaction.date = date
+        transaction.category = category
+        transaction.save()
+
+        print("Transaction Edited !")
+        return JsonResponse({
+            "success": True,
+            "id": transaction.id,
+            "date": str(transaction.date),
+            "category": transaction.category,
+            "description": transaction.description,
+            "amount": transaction.amount
+        })
+        
+    return JsonResponse({"success": False}, status=400)
+
+        
 
 def transactions_delete(request):
     if (request.method == "POST"):
@@ -48,4 +81,7 @@ def transactions_delete(request):
         tObject = Transactions.objects.get(id = transaction_id)
         tObject.delete()
         return JsonResponse({"success": True, "id": transaction_id})
+
+def investments_view(request):
+    return render(request, 'core/investments.html')
         
